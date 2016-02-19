d1e1c20b62c02c1ffdc05ed67c9516f775a2fff6
Alexander <alexchasejones@icloud.com>
2016-02-18 20:26:04 -0500

    remove scale

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-	var size = [window.width,window.height];
-	$(window).resize(function(){
-    	window.resizeTo(size[0],size[1]);
-	});
 
 	var sideFrameButton = document.getElementById("sideBar-button");
 	var sideFrame = document.getElementById("sideBar");
